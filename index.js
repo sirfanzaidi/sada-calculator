9c96bc553d78011213308496788279ae6c4f12b2
Irfan Hussain <I.hussain@expressmedia.int>
2024-03-25 12:23:50 +0500

    my very sada calculator

---
+++ index.js
@@ -0,0 +1,24 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    { message: "enter first number", type: "number", name: "firstNumber" },
+    { message: "enter second number", type: "number", name: "secondNumber" },
+    { message: "select one the operator to perform action",
+        type: "list",
+        name: "operator",
+        choices: ["addition", "subtration", "multiplication", "division"] }
+]);
+if (answer.operator === "addition") {
+    console.log(answer.firstNumber + answer.secondNumber);
+}
+else if (answer.operator === "subtration") {
+    console.log(answer.firstNumber - answer.secondNumber);
+}
+else if (answer.operator === "multiplication") {
+    console.log(answer.firstNumber * answer.secondNumber);
+}
+else if (answer.operator === "division") {
+    console.log(answer.firstNumber / answer.secondNumber);
+}
+else
+    (console.log, "please select wright choice");
